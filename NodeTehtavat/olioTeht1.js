34d4910ed451e20fa733ec36b50e77d6c897dcff
gashiAdrian <gashiad@outlook.com>
2023-04-05 22:28:12 +0300

    done NodeTehtavat

---
+++ NodeTehtavat/olioTeht1.js
@@ -0,0 +1,13 @@
+let henkilo = {
+    nimi: {
+      etu: "Adrian",
+      suku: "Gashi",
+      lempinimi: "Addu"
+    },
+    ika: 29,
+    kotikaupunki: "Espoo",
+    mielikirja: "Harry Potter",
+    lemmikit: ["Luna", "Lola"],
+    syntymaaika: "28.12.1993",
+    mieliruoka: "Lasagne",
+  };
---
+++ olioTeht2.js
@@ -0,0 +1,13 @@
+let favoriteMovies = {
+    nimi: "The Shawshank Redemption",
+    ohjaaja: "Frank Darabont",
+    valmistumisvuosi: 1994,
+    arviointi: 9.3,
+    näyttelijät: [
+        "Tim Robbins",
+        "Morgan Freeman",
+        "Bob Gunton",
+        "William Sadler",
+        "Clancy Brown",
+    ]
+};
